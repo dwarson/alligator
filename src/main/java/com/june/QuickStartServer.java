7f30c7c8782f2864898ffbd74259522934210aab
dwarson <dwarson@163.com>
2014-11-11 23:20:05 -0500

    fix jetty embedded server

--- src/main/java/com/june/QuickStartServer.java
+++ src/main/java/com/june/QuickStartServer.java
@@ -8,26 +8,32 @@ import org.eclipse.jetty.server.Server;
 
 public class QuickStartServer {
 
-    public static final int PORT = 8081;
-    public static final String CONTEXT = "/";
+	public static final int PORT = 8081;
+	public static final String CONTEXT = "/";
+	public static final String[] TLD_JAR_NAMES = new String[] { "spring-webmvc" };
 
-    public static void main(String[] args) throws Exception {
-        Server server = JettyFactory.createServerInSource(PORT, CONTEXT);
-        try {
-            server.start();
+	public static void main(String[] args) throws Exception {
+//		Profiles.setProfileAsSystemProperty(Profiles.DEVELOPMENT);
 
-            System.out.println("[INFO] Server running at http://localhost:" + PORT + CONTEXT);
-            System.out.println("[HINT] Hit Enter to reload the application quickly");
+		Server server = JettyFactory.createServerInSource(PORT, CONTEXT);
+		JettyFactory.setTldJarNames(server, TLD_JAR_NAMES);
+		try {
+			server.start();
 
-            while (true) {
-                char c = (char) System.in.read();
-                if (c == '\n') {
-                    JettyFactory.reloadContext(server);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(-1);
-        }
-    }
+			System.out.println("[INFO] Server running at http://localhost:"
+					+ PORT + CONTEXT);
+			System.out
+					.println("[HINT] Hit Enter to reload the application quickly");
+
+			while (true) {
+				char c = (char) System.in.read();
+				if (c == '\n') {
+					JettyFactory.reloadContext(server);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
 }
